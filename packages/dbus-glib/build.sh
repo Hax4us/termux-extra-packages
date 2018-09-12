bfd3da2bf1c93bba7fd1f287278dc8a0d0bf56b3
Hax4us <lkpandey950@gmail.com>
2018-09-12 16:42:35 +0530

    new package dbus-glib

---
+++ packages/dbus-glib/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/DBusBindings
+TERMUX_PKG_VERSION=0.110
+TERMUX_PKG_SRCURL=https://dbus.freedesktop.org/releases/dbus-glib/dbus-glib-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=7ce4760cf66c69148f6bd6c92feaabb8812dee30846b24cd0f7395c436d7e825
+TERMUX_PKG_DEPENDS="libdbus, glib"

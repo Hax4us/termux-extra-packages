fe0f19e826e83b614612ff3c4e38d27f686e58d9
Hax4us <lkpandey950@gmail.com>
2018-09-13 14:03:01 +0530

    add maintainer

--- packages/xfconf/build.sh
+++ packages/xfconf/build.sh
@@ -2,6 +2,7 @@ TERMUX_PKG_HOMEPAGE=http://www.xfce.org/
 TERMUX_PKG_DESCRIPTION="simple client-server configuration storage and query system"
 _MAJOR_VERSION=4.12
 TERMUX_PKG_VERSION=${_MAJOR_VERSION}.1
+TERMUX_PKG_MAINTAINER="lokesh @hax4us"
 TERMUX_PKG_SRCURL=http://archive.xfce.org/src/xfce/xfconf/${_MAJOR_VERSION}/xfconf-${TERMUX_PKG_VERSION}.tar.bz2
 TERMUX_PKG_SHA256=35f48564e5694faa54fdc180cd3268e16fc2352946a89a3c2fc12cbe400ada36
 TERMUX_PKG_DEPENDS="libxfce4util, dbus-glib"
